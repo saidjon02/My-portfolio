e8337f6b95b705b0db56e5505abed6ec40aaaccc
Saidjon <fozilovsaidjon71@gmail.com>
2024-07-19 11:17:46 +0500

    hello

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,43 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import "boxicons";
+import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="header container-h font">
-      <div className="head-left">
-        <img className="logo" src={logo} alt="" />
-        <i class="bx bx-menu"></i>
-      </div>
-      <div className="head-right">
-        <p className="head-link">
-          <span className="green">01. </span>About
-        </p>
-        <p className="head-link">
-          <span className="green">02. </span>Experience
-        </p>
-        <p className="head-link">
-          <span className="green">03. </span>Work
-        </p>
-        <p className="head-link">
-          <span className="green">04. </span>Contact
-        </p>
-        <button className="green-btn">Resume</button>
-      </div>
+      <img className="logo" src={logo} alt="" />
+      <nav className="nav">
+        <div className="nav2">
+          <ul>
+            <li>
+              <Link className="head-link">
+                {" "}
+                <span className="green">01. </span>About
+              </Link>
+            </li>
+            <li>
+              <Link className="head-link">
+                <span className="green">02. </span>Experience
+              </Link>
+            </li>
+            <li>
+              <Link className="head-link">
+                <span className="green">03. </span>Work
+              </Link>
+            </li>
+            <li>
+              <Link className="head-link">
+                <span className="green">04. </span>Contact
+              </Link>
+            </li>
+            <li>
+              <Link className="head-link"></Link>
+            </li>
+          </ul>
+          <button className="green-btn">Resume</button>
+        </div>
+      </nav>
+      <box-icon name="menu" size={"md"} color="white"></box-icon>
     </div>
   );
 }
