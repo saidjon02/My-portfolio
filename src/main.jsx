aef3f2df8c75c039e5cda9ca9055c0239cab9977
saidjon02 <fozilovsaidjon71@gmail.com>
2024-08-17 11:05:00 +0300

    provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Helmet } from "react-helmet";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
